4d6e612ede0cbca01ded2231a73e1bb35616341e
ssalma2002 <syoussef4444@gmail.com>
2025-09-14 21:57:41 +0300

    initial main and header files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"please enter a number"<<endl;
+    cin>>num;
+    if(num ==1){
+        add()
+    }
+    else if(num ==2){
+        subtract()
+    }
+    else if(num ==3){
+        multiply()
+    }
+    else if(num ==4){
+        divide()
+    }
+}
